1d93534389c533186de64312d4e47bf2ae2b7c91
Joao Victor Rodrigues de Souza <victorrodrigues.desenvolvimento@gmail.com>
2021-02-01 12:58:01 -0300

    add and config typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
 
+import './database/connection';
 import routes from './routes';
 
 const app = express();
 
-app.use(express.json())
-app.use(routes)
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => console.log('Server on fire in http://localhost:3000'));
