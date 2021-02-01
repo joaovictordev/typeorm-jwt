01f70e61fcfe0576c1bccaf968b11e2c764f23ae
Joao Victor Rodrigues de Souza <victorrodrigues.desenvolvimento@gmail.com>
2021-02-01 01:54:47 -0300

    add routes file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello' });
-});
+app.use(express.json())
+app.use(routes)
 
-app.listen(3333, () => console.log('Server on fire in port 3333'));
+app.listen(3000, () => console.log('Server on fire in http://localhost:3000'));
