e80d1ef9af52cdd5a91d404da6af204db123ddb5
Joao Victor Rodrigues de Souza <victorrodrigues.desenvolvimento@gmail.com>
2021-02-01 13:54:12 -0300

    add bcrypt to hash password

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,15 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate } from 'typeorm';
+import bcrypt from 'bcryptjs';
 
 @Entity('users')
 class User {
   @PrimaryGeneratedColumn('uuid')
-  id: String;
+  id: string;
 
   @Column()
-  email: String;
+  email: string;
 
   @Column()
-  password: String;
+  password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
 }
 
 export default User;
